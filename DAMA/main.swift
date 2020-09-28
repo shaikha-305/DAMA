9d37ee12fb4985b6c03805c39a86c372511f0e60
shaikha-305 <shaikhaaljenaidel@gmail.com>
2020-09-28 07:54:41 +0300

    Initial Commit

---
+++ DAMA/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DAMA
+//
+//  Created by shaikha aljenaidel on 9/28/20.
+//  Copyright © 2020 shaikha aljenaidel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
